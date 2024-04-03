704617fe3ec14c31bf9c8eb78b6081c97770460e
sungmin choi <namja306@naver.com>
2024-04-03 14:20:55 +0900

    fix

--- components/sections/featuredPost/index.tsx
+++ components/sections/featuredPost/index.tsx
@@ -7,9 +7,7 @@ const FeaturedPost = () => {
       <div className="container mx-auto flex flex-col md:flex-row md:items-center gap-4 px-4 md:px-10">
         <div className="w-[85%] md:w-1/2 pb-6 md:pb-0">
           <p>January 01, 2023</p>
-          <h2 className="text-2xl font-bold">
-            Code Fuel: The Importance of Food for Software Developers
-          </h2>
+          <h2 className="text-2xl font-bold">QWER BLOG</h2>
 
           <Link href="/posts/food-for-software-devs" prefetch={false}>
             <a className="rounded-lg py-2 px-4 text-white bg-pink-700 mt-10">
